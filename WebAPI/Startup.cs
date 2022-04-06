3786bc5505e2309dc54ea7647ee8a90791d06e72
Can <mccankaya45@hotmail.com>
2022-04-06 05:12:57 +0300

    Day 11

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -36,13 +36,13 @@ namespace WebAPI
             services.AddSingleton<IColorService, ColorManager>();
             services.AddSingleton<ICustomerService, CustomerManager>();
             services.AddSingleton<IRentalService, RentalManager>();
-            //services.AddSingleton<IUserService, UserManager>();
+            services.AddSingleton<IUserService, UserManager>();
             services.AddSingleton<ICarDal, EfCarDal>();
             services.AddSingleton<IBrandDal,EfBrandDal>();
             services.AddSingleton<IColorDal,EfColorDal>();
             services.AddSingleton<ICustomerDal, EfCustomerDal>();
             services.AddSingleton<IRentalDal, EfRentalDal>();
-            //services.AddSingleton<IUserDal, EfUserDal>();
+            services.AddSingleton<IUserDal, EfUserDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
