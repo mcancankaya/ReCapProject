0ea23a21ea39d7543e9a7b2f2e019c22b5aed61c
Can <mccankaya45@hotmail.com>
2022-04-14 02:55:25 +0300

    .

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //builder.RegisterType<>
+        }
+    }
+}
